13583ce83167f8169418e18562992f9c3a2ad4a1
Mark Sanford <marktsanford@gmail.com>
2021-10-06 12:01:23 -0700

    preview panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import './App.global.scss';
+import React from "react";
+import "./App.global.scss";
 // import './ImageRectTool.css';
 
-import ImageRectTool from './components/ImageRectTool';
-
+import ImageRectTool from "./components/ImageRectTool";
+import Controls from "./components/Controls";
+import SplitterLayout from 'react-splitter-layout';
+import ImageEssentialGrid from "./components/ImageEssentialGrid";
 
 function App() {
+  const actionHandler: any = (event: any) => {
+
+  };
+
   return (
-    <div className="App">
+    <SplitterLayout
+      customClassName="imgjoy-splitter-layout"
+      primaryMinSize={100}
+      secondaryMinSize={100}
+    >
+      <div className="left-panel">
+        <Controls onAction={actionHandler} />
+        <div className="essential-grid-wrapper">
+          <ImageEssentialGrid />
+        </div>
+      </div>
       <ImageRectTool />
-    </div>
+    </SplitterLayout>
   );
 }
 
