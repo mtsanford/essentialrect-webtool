3eb00bd8799d1c253ddfb5ffd6519171e36fb791
Mark Sanford <marktsanford@gmail.com>
2021-10-21 16:12:04 -0700

    use new react-essentialrect-editor

--- src/components/ImageRectTool.tsx
+++ src/components/ImageRectTool.tsx
@@ -1,59 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { CSSProperties, useEffect } from "react";
 
-import EssentialRectEditor from 'react-essentialrect-editor';
-import { Rect } from 'react-essentialrect';
+import EssentialRectEditor from "react-essentialrect-editor";
+import { Rect, useClientRect, fitRect } from "react-essentialrect";
 
-import ImageViewerControls from './ImageViewerControls';
-import ImageViewerInfo from './ImageViewerInfo';
+import ImageViewerControls from "./ImageViewerControls";
+import ImageViewerInfo from "./ImageViewerInfo";
 
-import { useAppSelector, useAppDispatch } from '../store/hooks';
-import {
-  selectMinAspectRatio,
-  selectMaxAspectRatio,
-} from '../store/ui-slice';
+import { useAppSelector, useAppDispatch } from "../store/hooks";
+import { selectMinAspectRatio, selectMaxAspectRatio } from "../store/ui-slice";
 import {
   currentImageActions,
   selectCurrentImage,
-} from '../store/current-image-slice';
+} from "../store/current-image-slice";
 
 const ImageRectTool = () => {
+  const [editorWrapperRef, editorWrapperRect] = useClientRect();
   const dispatch = useAppDispatch();
   const minAspectRatio = useAppSelector(selectMinAspectRatio);
   const maxAspectRatio = useAppSelector(selectMaxAspectRatio);
-  const { url, essentialRect } = useAppSelector(
-    selectCurrentImage
-  );
+  const { url, essentialRect, imageRect } = useAppSelector(selectCurrentImage);
+  let editorStyles: CSSProperties = {};
 
-  const resetEssentialRect = () => {
-    dispatch(currentImageActions.resetEssentialRect({minAspectRatio, maxAspectRatio}));
+  if (url && editorWrapperRect && imageRect) {
+    const editorRect = fitRect(imageRect, imageRect, editorWrapperRect);
+
+    console.log(editorWrapperRect);
+
+    editorStyles = {
+      top: `${editorRect.top}px`,
+      left: `${editorRect.left}px`,
+      width: `${editorRect.width}px`,
+      height: `${editorRect.height}px`,
+      position: "relative",
+    };
   }
 
+  const resetEssentialRect = () => {
+    dispatch(
+      currentImageActions.resetEssentialRect({ minAspectRatio, maxAspectRatio })
+    );
+  };
+
   useEffect(() => {
-    dispatch(currentImageActions.resetEssentialRect({minAspectRatio, maxAspectRatio}));
+    dispatch(
+      currentImageActions.resetEssentialRect({ minAspectRatio, maxAspectRatio })
+    );
   }, [minAspectRatio, maxAspectRatio, dispatch]);
 
   const essentialRectChanged = (newEssentialRect: Rect) => {
     dispatch(currentImageActions.setEssentialRect(newEssentialRect));
-  }
+  };
 
   const onImageLoaded = (element: HTMLImageElement) => {
     if (!essentialRect) {
-      const imageRect  = { top: 0, left: 0, width: element.naturalWidth, height: element.naturalHeight};
-      dispatch(currentImageActions.resetEssentialRect({minAspectRatio, maxAspectRatio, imageRect}));
+      const imageRect = {
+        top: 0,
+        left: 0,
+        width: element.naturalWidth,
+        height: element.naturalHeight,
+      };
+      dispatch(
+        currentImageActions.resetEssentialRect({
+          minAspectRatio,
+          maxAspectRatio,
+          imageRect,
+        })
+      );
     }
-  }
+  };
 
   return (
     <div className="image-rect-tool">
       <ImageViewerControls onReset={resetEssentialRect} />
-      <EssentialRectEditor
-        imageUrl={url}
-        essentialRect={essentialRect}
-        onEssentialRectChange={essentialRectChanged}
-        onImageLoaded={onImageLoaded}
-        minAspectRatio={minAspectRatio}
-        maxAspectRatio={maxAspectRatio}
-      />
+
+      <div className="EditorBorder"ref={editorWrapperRef}>
+        <div className="EditorWrapper">
+          {url && (
+            <EssentialRectEditor
+              imageUrl={url}
+              essentialRect={essentialRect}
+              onEssentialRectChange={essentialRectChanged}
+              onImageLoaded={onImageLoaded}
+              minAspectRatio={minAspectRatio}
+              maxAspectRatio={maxAspectRatio}
+              style={editorStyles}
+            />
+          )}
+        </div>
+      </div>
+
       <ImageViewerInfo />
     </div>
   );
